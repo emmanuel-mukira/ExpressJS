2cf4d314a2777c4bc3be88f16a900f9d3cbd5572
emmanuel-mukira <emmanuelgichigo@gmail.com>
2024-08-06 11:00:56 +0300

    INTRO & GET requests

--- src/index.js
+++ src/index.js
@@ -10,7 +10,28 @@ const PORT=3001;
 //logging
 app.listen(PORT,()=>console.log(`Running express server on port ${PORT}!`));
 
+//GET REQUEST
 //Call get method on app takes 2 parameters
 //1st one is the path its is mandatory
-//
-app.get('')
+//2nd one is a callback function taking two arguments 
+//first is the request object , second is response object
+//request gives all information about client making request stuff like cookies,headers ...
+//response is responsible for handling sending responses to clients
+app.get('/groceries',(request,response)=>{
+    //sending body back to client
+    response.send([
+     {
+       item: 'milk',
+       quantity: 2,
+     },
+     {
+       item: 'bread',
+       quantity:6,
+     },
+     {
+       item: 'cookies',
+       quantity:7,
+     }
+    ])
+});
+
